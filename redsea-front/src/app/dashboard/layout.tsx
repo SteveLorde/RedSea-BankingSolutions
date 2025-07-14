1d812fcf0e5c724208ecac0484b08b61b85db28d
Mosta <mostafa.maher98@gmail.com>
2025-07-14 20:55:33 +0300

    1- Moved totalMoneyChart outside general components folder to specific route based components 2- Created Footer component

--- redsea-front/src/app/dashboard/layout.tsx
+++ redsea-front/src/app/dashboard/layout.tsx
@@ -1,16 +1,16 @@
 import Navbar from "@/components/dashboard/navbar/navbar";
+import Footer from "@/components/dashboard/footer/footer";
 
-export default function DashboardLayout({children}: {
-    children: React.ReactNode
-}) {
-    return (
-        <html>
-        <body>
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html>
+      <body>
         <main>
-            <Navbar/>
-            {children}
+          <Navbar />
+          {children}
+          <Footer />
         </main>
-        </body>
-        </html>
-    )
+      </body>
+    </html>
+  );
 }
